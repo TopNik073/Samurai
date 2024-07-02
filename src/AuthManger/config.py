e1b24b38748a59dbeb713c529e1fbeb600c764b5
TopNik073 <borzovn169@gmail.com>
2024-07-02 15:31:49 +0200

    Add lifetime for tokens

--- src/AuthManger/config.py
+++ src/AuthManger/config.py
@@ -6,6 +6,8 @@ class JWTConfig:
     def __init__(self, JWT_SECRET_KEY: str, JWT_ALGORITHM: str):
         self.JWT_SECRET_KEY = JWT_SECRET_KEY
         self.JWT_ALGORITHM = JWT_ALGORITHM
+        self.JWT_ACCESS_TOKEN_EXPIRE = 600
+        self.JWT_REFRESH_TOKEN_EXPIRE = 60 * 60 * 24 * 30
 
 
 load_dotenv()
